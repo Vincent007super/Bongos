3f4f4caa2868b0e35d5bd875af8302cd589c0678
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2024-06-21 14:46:55 +0200

    Added header
    - css linked in site.css
    - is template for all pages
    - searchbar not functional (yet)

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test</title>
-    <link rel="stylesheet" href="../style/style.css">
-    <link rel="stylesheet" href="../style/site.css">
+    <title>Hoofdpagina</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/site.css">
 </head>
+
 <body>
-    
+    <header>
+        <div id="menu_pos" onclick="menuOpen()">
+            &#9776;
+        </div>
+        <div id="logo">
+            <h3>De Culinaire Crew</h3>
+        </div>
+        <div id="searcher">
+            <input type="search" name="Searchbar" id="searcher" placeholder="Zoek uw favoeriete recept op!">
+        </div>
+        <div id="buttons">
+            <button id="inlog">
+                Inloggen
+            </button>
+            <button id="aboneer">
+                Aboneren
+            </button>
+        </div>
+    </header>
+
+    <main>
+    <script src="script/script.js"></script>
 </body>
+
 </html>
